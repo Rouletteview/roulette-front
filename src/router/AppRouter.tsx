c7f9801a8e9c7f53585c37d768fb69bc63cf0680
Alexcab24 <sasuke246473@gmail.com>
2025-02-24 15:28:52 -0400

    login routing

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router"
 import LandingPage from "../pages/LandingPage"
 import RegisterPage from "../pages/RegisterPage"
+import LoginPage from "../pages/LoginPage"
 
 
 
@@ -9,6 +10,8 @@ const AppRouter = () => {
    <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/registrarse" element={<RegisterPage />} />
+      <Route path="/iniciar-sesion" element={<LoginPage />} />
+
 
    </Routes>
   )
