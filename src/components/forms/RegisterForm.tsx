470b804afe8df97cc2b75c4a7b0702a58230b5e3
Alexcab24 <sasuke246473@gmail.com>
2025-02-24 15:28:10 -0400

    register form working

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form"
 import { RegisterFormData, registerSchema } from "../../schemas/registerSchema"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useRegister } from "../../hooks/useRegister";
+import Input from "./Input";
 
 
 const RegisterForm = () => {
@@ -19,18 +20,18 @@ const RegisterForm = () => {
             const [name, ...lastnameArray] = data.fullName.trim().split(" ");
             const lastname = lastnameArray.join(" ") || "N/A";
 
-    
+
             const result = await registerUser({
                 variables: {
-                  FirstName: name,
-                  LastName: lastname,
-                  Password: data.password,
-                  Email: data.email,
-                  Country: data.country,
-                  BirthDate: new Date(data.birthDate).toISOString(),
-                  PhoneNumber: data.phone.toString(),
+                    FirstName: name,
+                    LastName: lastname,
+                    Password: data.password,
+                    Email: data.email,
+                    Country: data.country,
+                    BirthDate: new Date(data.birthDate).toISOString(),
+                    PhoneNumber: data.phone,
                 },
-              });
+            });
             console.log("Usuario registrado:", result);
         } catch (error) {
             console.error("Error al registrar:", error);
@@ -42,66 +43,50 @@ const RegisterForm = () => {
 
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="max-w-sm space-y-3">
-                <input
-                    type="text"
-                    placeholder="Nombre completo"
-                    {...register('fullName', { required: 'El nombre es requerido' })}
-                    className="py-3 px-4 block w-full rounded-lg text-sm border border-black focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-                />
-                {errors.fullName && <p className="text-red-500 text-sm">{errors.fullName.message}</p>}
-            </div>
+        <form onSubmit={handleSubmit(onSubmit)} >
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-6">
+            <Input
+                placeholder="Nombre completo"
+                name="fullName"
+                register={register("fullName")}
+                error={errors.fullName?.message}
+            />
+            <Input
+                placeholder="Fecha de nacimiento"
+                name="birthdate"
+                type="date"
+                register={register("birthDate")}
+                error={errors.birthDate?.message}
+            />
+            <Input
+                placeholder="Teléfono"
+                name="phone"
+                register={register("phone")}
+                error={errors.phone?.message}
+            />
 
-            <div className="max-w-sm space-y-3">
-                <input
-                    type="date"
-                    placeholder="Fecha de nacimiento"
-                    {...register('birthDate', { required: 'Fecha de nacimiento es requerida' })}
-                    className="py-3 px-4 block w-full rounded-lg text-sm border border-black focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-                />
-                {errors.birthDate && <p className="text-red-500 text-sm">{errors.birthDate?.message}</p>}
-            </div>
+            <Input
+                placeholder="País"
+                name="country"
+                register={register("country")}
+                error={errors.country?.message}
+            />
 
-            <div className="max-w-sm space-y-3">
-                <input
-                    type="text"
-                    placeholder="Teléfono"
-                    {...register('phone', { required: 'El teléfono es requerido' })}
-                    className="py-3 px-4 block w-full rounded-lg text-sm border border-black focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-                />
-                {errors.phone && <p className="text-red-500 text-sm">{errors.phone.message}</p>}
+            <Input
+                placeholder="Correo electrónico"
+                name="email"
+                register={register("email")}
+                error={errors.email?.message}
+            />
+            <Input
+                placeholder="Contraseña"
+                type="password"
+                name="password"
+                register={register("password")}
+                error={errors.password?.message}
+            />
             </div>
-
-            <div className="max-w-sm space-y-3">
-                <input
-                    type="text"
-                    placeholder="País"
-                    {...register('country', { required: 'El país es requerido' })}
-                    className="py-3 px-4 block w-full rounded-lg text-sm border border-black focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-                />
-                {errors.country && <p className="text-red-500 text-sm">{errors.country.message}</p>}
-            </div>
-
-            <div className="max-w-sm space-y-3">
-                <input
-                    type="text"
-                    placeholder="Correo electrónico"
-                    {...register('email', { required: 'El correo es requerido' })}
-                    className="py-3 px-4 block w-full rounded-lg text-sm border border-black focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-                />
-                {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
-            </div>
-
-            <div className="max-w-sm space-y-3">
-                <input
-                    type="password"
-                    placeholder="Contraseña"
-                    {...register('password', { required: 'La contraseña es requerida' })}
-                    className="py-3 px-4 block w-full rounded-lg text-sm border border-black focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
-                />
-                {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
-            </div>
+          
 
             <button type="submit" className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
                 Registrarse
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,11 +1,26 @@
 import RegisterForm from "../components/forms/RegisterForm"
+import Header from "../components/Header"
+import img from "../assets/images/phone-auth.png"
 
 
 const RegisterPage = () => {
   return (
-    <main className="flex justify-center items-center h-screen">
-      <RegisterForm/>
-    </main>
+    <>
+      <Header>
+        <a href="/" className="border bg-[#D9A425] hover:bg-[#B3831D] transition-all px-4 py-2 rounded-xl text-center">Ir al inicio</a>
+      </Header>
+      <main className="flex flex-col justify-center items-center h-screen mx-6 mt-27">
+        <div className="text-center my-8">
+          <h1 className="text-lg leading-6 font-bold"><span className="text-[#D9A425] font-medium">¡Únete a nuestra comunidad de jugadores inteligentes!</span> Crea tu cuenta ahora para acceder a las herramientas más avanzadas de análisis de la ruleta</h1>
+        </div>
+        <div className="w-full">
+          <RegisterForm />
+        </div>
+        <div className="absolute bottom-0 right-0 ">
+          <img src={img} alt="" className="w-[565px] h-auto hidden md:block" />
+        </div>
+      </main>
+    </>
   )
 }
 
