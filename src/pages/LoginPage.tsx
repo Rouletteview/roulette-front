b3f3bbadaca2d1313b11ce7e4c66d16b0ac80ea0
Alexcab24 <sasuke246473@gmail.com>
2025-02-24 15:28:26 -0400

    login-page

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../components/forms/LoginForm"
+
+
+const LoginPage = () => {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <LoginForm/>
+    </main>
+  )
+}
+
+export default LoginPage
