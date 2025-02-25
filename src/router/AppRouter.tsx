b1ad3bc9f6f0e6be48ba84649d5d5a77ff15042f
Alexcab24 <sasuke246473@gmail.com>
2025-02-25 19:40:40 -0400

    email-check router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router"
 import LandingPage from "../pages/LandingPage"
 import RegisterPage from "../pages/RegisterPage"
 import LoginPage from "../pages/LoginPage"
+import CheckEmailPage from "../pages/CheckEmailPage"
 
 
 
@@ -11,6 +12,9 @@ const AppRouter = () => {
       <Route path="/" element={<LandingPage />} />
       <Route path="/registrarse" element={<RegisterPage />} />
       <Route path="/iniciar-sesion" element={<LoginPage />} />
+
+
+      <Route path="/confirmacion-correo" element={<CheckEmailPage/>} />
 
 
    </Routes>
