f1a6559e3636ff06cfeb152e2cd0962e12fb9579
Alexcab24 <sasuke246473@gmail.com>
2025-02-25 19:41:31 -0400

    email-check redirect

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -4,9 +4,14 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useRegister } from "../../hooks/useRegister";
 import Input from "./Input";
 
+import arrowIcon from "../../assets/icon/arrow-down-white.svg"
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 
 const RegisterForm = () => {
-
+    const [isChecked, setIsChecked] = useState(false);
+    const navigate = useNavigate();
     const { register, handleSubmit, formState: { errors } } = useForm<RegisterFormData>({
         resolver: zodResolver(registerSchema),
     });
@@ -33,6 +38,9 @@ const RegisterForm = () => {
                 },
             });
             console.log("Usuario registrado:", result);
+            navigate("/confirmacion-correo");
+
+
         } catch (error) {
             console.error("Error al registrar:", error);
         }
@@ -41,57 +49,98 @@ const RegisterForm = () => {
 
 
 
-
     return (
-        <form onSubmit={handleSubmit(onSubmit)} >
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-y-6">
-            <Input
-                placeholder="Nombre completo"
-                name="fullName"
-                register={register("fullName")}
-                error={errors.fullName?.message}
-            />
-            <Input
-                placeholder="Fecha de nacimiento"
-                name="birthdate"
-                type="date"
-                register={register("birthDate")}
-                error={errors.birthDate?.message}
-            />
-            <Input
-                placeholder="Teléfono"
-                name="phone"
-                register={register("phone")}
-                error={errors.phone?.message}
-            />
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full  px-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input
+                    placeholder="Nombre completo"
+                    name="fullName"
+                    register={register("fullName")}
+                    error={errors.fullName?.message}
+                />
+                <Input
+                    placeholder="Fecha de nacimiento"
+                    name="birthDate"
+                    type="date"
+                    register={register("birthDate")}
+                    error={errors.birthDate?.message}
+                />
 
-            <Input
-                placeholder="País"
-                name="country"
-                register={register("country")}
-                error={errors.country?.message}
-            />
 
-            <Input
-                placeholder="Correo electrónico"
-                name="email"
-                register={register("email")}
-                error={errors.email?.message}
-            />
-            <Input
-                placeholder="Contraseña"
-                type="password"
-                name="password"
-                register={register("password")}
-                error={errors.password?.message}
-            />
+                <div className="flex gap-2 w-full">
+                    <div className=" max-w-[95px]"  >
+                        <Input
+                            placeholder="+00"
+                            name="countryCode"
+                            // register={register("countryCode")}
+                            // error={errors.countryCode?.message}
+
+                            icon={arrowIcon}
+                        />
+                    </div>
+                    <div className="w-full">
+                        <Input
+                            placeholder="Teléfono"
+                            name="phone"
+                            register={register("phone")}
+                            error={errors.phone?.message}
+
+                        />
+                    </div>
+
+
+                </div>
+
+
+                <Input
+                    placeholder="País"
+                    name="country"
+                    register={register("country")}
+                    error={errors.country?.message}
+                    icon={arrowIcon}
+                />
+                <Input
+                    placeholder="Correo electrónico"
+                    name="email"
+                    register={register("email")}
+                    error={errors.email?.message}
+                />
+                <Input
+                    placeholder="Contraseña"
+                    type="password"
+                    name="password"
+                    register={register("password")}
+                    error={errors.password?.message}
+                />
             </div>
-          
 
-            <button type="submit" className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
-                Registrarse
-            </button>
+            <div className="mt-5 md:mt-11 flex flex-col items-center justify-center">
+
+                <div className="flex gap-x-2">
+                    <label className="text-sm md:text-lg text-white">
+                        ¿Confirmas que cumples con la mayoría de edad para el uso de nuestra plataforma?
+                    </label>
+                    <input
+                        type="checkbox"
+                        className="rounded text-white focus:ring-[#D9A425] accent-[#D9A425]"
+                        onChange={(e) => setIsChecked(e.target.checked)}
+                        required
+                    />
+                </div>
+
+
+
+                <button
+                    type="submit"
+                    disabled={!isChecked}
+                    className="block bg-[#D9A425] hover:bg-[#B3831D] transition-all w-full md:w-[512px] text-lg font-bold rounded-[10px] py-2 mt-5 mb-24 disabled:bg-[#B2B2B2]"
+                >
+                    Registrarse
+                </button>
+            </div>
+
         </form>
+
     )
 }
 
