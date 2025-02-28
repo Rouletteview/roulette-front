378ea0f9cf1257d6358b2bb12342e9d762b32180
Alexcab24 <sasuke246473@gmail.com>
2025-02-28 15:48:19 -0400

    auth-background image

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -16,14 +16,14 @@ const LoginPage = () => {
       </Header>
       <section
         className="w-full  bg-center bg-cover flex items-center justify-center text-white relative"
-        style={{ backgroundImage: "url('/background/close-up-roulette-wheel-1.png')" }}
+        style={{ backgroundImage: "url('/background/view-roulette-casino.png')" }}
       >
-        <div className="w-full min-h-screen grid lg:grid-cols-3 justify-center items-center px-6 pt-11 hero-background ">
-          
+        <div className="w-full min-h-screen grid lg:grid-cols-3 justify-center items-center px-6 pt-11  ">
+
           <div className="   col-span-2 lg:px-28  xl:px-48">
             <div className="text-center my-8">
               <h1 className="text-[28px] md:text-[32px] text-white leading-8 md:leading-12 font-medium">
-              Inicia sesión para acceder a los datos que necesitas para ganar
+                Inicia sesión para acceder a los datos que necesitas para ganar
               </h1>
             </div>
 
@@ -32,12 +32,17 @@ const LoginPage = () => {
             </div>
           </div>
 
-          <div className="absolute bottom-0 right-0 hidden lg:block">
-            <img
-              src={img}
-              alt="Ilustración"
-              className="w-[250px] md:w-[350px] lg:w-[450px] xl:w-[500px] h-auto absolute bottom-0 right-0 lg:relative"
-            />
+          <div className="absolute bottom-0 right-0 hidden lg:block ">
+            <div className="relative z-10 drop-shadow-[0_0_25px_rgba(217, 164, 37, 1)]
+      before:content-[''] before:absolute before:-left-14 before:top-12 before:w-[177px] md:before:w-[250px] before:h-[169px] md:before:h-[272px] before:bg-[#D9A425] before:blur-[100px]  before:rounded-full before:-z-10
+      after:content-[''] after:absolute after:right-5 after:top-40 after:w-[210px] after:h-[200px] after:bg-[#D9A425] after:blur-[100px] after:rounded-full after:-z-10">
+              <img
+                src={img}
+                alt="Ilustración"
+                className="w-[250px] md:w-[350px] lg:w-[450px] xl:w-[500px] h-auto absolute bottom-0 right-0 lg:relative"
+              />
+            </div>
+
           </div>
         </div>
       </section>
