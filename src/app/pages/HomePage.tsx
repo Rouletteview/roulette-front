9b3302c5c6a064b7dba576cf7c38158491f1ad42
Alexcab24 <sasuke246473@gmail.com>
2025-04-12 14:49:07 -0400

    Refactor HomePage component to integrate AppLayout, replacing Header and NavBar for improved structure; streamline layout with HeroSection and ChartSection components.

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,7 +1,7 @@
-import Header from "../../components/Header"
+
 import HeroSection from "../../sections/HeroSection"
 import WelcomeModal from "../components/Modal/welcome-modal/WelcomeModal"
-import NavBar from "../components/NavBar"
+import AppLayout from "../layouts/AppLayout"
 import ChartSection from "../sections/ChartSection"
 
 
@@ -12,33 +12,33 @@ const HomePage = () => {
   return (
     <>
       <WelcomeModal />
-      <Header>
-        <NavBar />
-      </Header>
-      <HeroSection imageURL='/background/home-background.png'>
-        <div className='text-start mx-7 lg:mx-24'>
-          <div className='flex flex-col justify-between gap-y-8 h-full'>
-            <div className='max-w-4xl'>
-              <h1 className='text-xl lg:text-7xl font-semibold leading-7 lg:leading-20'><span className='text-[#D9A425]'>¡Analiza y gana! </span>con nuestros gráficos en tiempo real del juego de la <span className='text-[#D9A425]'>ruleta de casino</span> </h1>
+      <AppLayout>
+        <HeroSection imageURL='/background/home-background.png'>
+          <div className='text-start mx-7 lg:mx-24'>
+            <div className='flex flex-col justify-between gap-y-8 h-full'>
+              <div className='max-w-4xl'>
+                <h1 className='text-xl lg:text-7xl font-semibold leading-7 lg:leading-20'><span className='text-[#D9A425]'>¡Analiza y gana! </span>con nuestros gráficos en tiempo real del juego de la <span className='text-[#D9A425]'>ruleta de casino</span> </h1>
 
-            </div>
+              </div>
 
-            <a href=""
-              className="w-full lg:w-[390px]   bg-[#D9A425] hover:bg-[#B3831D] 
+              <a href=""
+                className="w-full lg:w-[390px]   bg-[#D9A425] hover:bg-[#B3831D] 
     text-center font-bold rounded-xl inset-shadow-2xs transition-all
     text-sm sm:text-base md:text-lg lg:text-xl
     px-4 sm:px-6 md:px-8 
     py-3 sm:py-4 
   
   ">
-              Comienza con la prueba gratuita
-            </a>
+                Comienza con la prueba gratuita
+              </a>
 
 
+            </div>
           </div>
-        </div>
-      </HeroSection>
-      <ChartSection />
+        </HeroSection>
+        <ChartSection />
+      </AppLayout>
+
     </>
 
   )
