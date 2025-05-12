1827f00c3c0b5d8f6389cc62afcada943bb24af7
Alexcab24 <sasuke246473@gmail.com>
2025-05-12 11:31:25 -0400

    dinamyc token

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,14 +1,24 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { useAuthStore } from "../stores/authStore";
-const token = useAuthStore.getState().token;
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_GRAPHQL_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = useAuthStore.getState().token;
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-
-  uri: import.meta.env.VITE_GRAPHQL_URL,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
 });
 
 export default client;
