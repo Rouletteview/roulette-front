54bd9e25f8a10063415ed70f4487318a67c5eec4
Alexcab24 <acabral2442@gmail.com>
2025-07-13 19:52:29 -0400

    Add index file for section exports and update import path for BetSection in ChartSection component

--- src/app/sections/ChartSection/ChartSection.tsx
+++ src/app/sections/ChartSection/ChartSection.tsx
@@ -11,7 +11,9 @@ import NumbersDisplay from '../../components/NumbersDisplay';
 import UserInfo from '../../components/UserInfo';
 import Update from '../../components/Update';
 import { FullscreenChartModal } from './components/FullscreenChartModal';
-import BetSection from '../BetSection/BetSection';
+import BetSection from '../BetSection';
+
+
 // import BetModal from '../../components/BetModal';
 
 function useContainerWidth() {
