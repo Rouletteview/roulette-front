993c68e9b1641fda631d650001ef28719b808bca
Alexcab24 <acabral2442@gmail.com>
2025-07-17 18:58:06 -0400

    overflow-bug-fixed

--- src/app/sections/ChartSection/components/ChartRenderer.tsx
+++ src/app/sections/ChartSection/components/ChartRenderer.tsx
@@ -110,8 +110,8 @@ export const ChartRenderer: React.FC<ChartRendererProps> = ({
 
     return (
         <Suspense fallback={<LoadingOverlay />}>
-            <div className="lg:cursor-default cursor-pointer relative group w-full max-w-full overflow-x-hidden">
-                <div className=" inset-0 bg-black/0 hover:bg-black/10 transition-all duration-200 lg:hidden pointer-events-none z-10 rounded-lg" />
+            <div className="lg:cursor-default cursor-pointer relative group w-full max-w-full overflow-hidden">
+                <div className=" inset-0 bg-black/0 hover:bg-black/10 transition-all duration-200 lg:hidden pointer-events-none z-10 rounded-lg " />
                 {renderChart()}
             </div>
         </Suspense>
